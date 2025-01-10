1e1187780607cd289a2ad42da8216ea76df3c83d
venomaus <stefvandenbruel@outlook.com>
2025-01-10 11:42:47 +0100

    Setup font + resolution based screensize

--- Roguelike/Constants.cs
+++ Roguelike/Constants.cs
@@ -6,5 +6,7 @@
     internal static class Constants
     {
         public const string GameTitle = "Roguelike";
+        public const string Font = "Fonts/Aesomatica_16x16.font";
+        public static (int width, int height) Resolution = (1920, 1080);
     }
 }
--- Roguelike/Program.cs
+++ Roguelike/Program.cs
@@ -1,4 +1,5 @@
-﻿using Roguelike.Screens;
+﻿using Newtonsoft.Json;
+using Roguelike.Screens;
 using SadConsole;
 using SadConsole.Configuration;
 
@@ -14,15 +15,30 @@ namespace Roguelike
             Settings.WindowTitle = Constants.GameTitle;
             Settings.ResizeMode = Settings.WindowResizeOptions.Fit;
 
-            Builder configuration = new Builder()
-                .SetScreenSize(90, 30)
+            Builder configuration = new();
+
+            configuration = configuration
                 .SetStartingScreen<ScreenContainer>()
                 .IsStartingScreenFocused(true)
-                .ConfigureFonts(true);
+                .ConfigureFonts((f, gh) =>
+                {
+                    f.UseCustomFont(Constants.Font);
+
+                    // Set screen size based on the font
+                    var (width, height) = DefineScreenSize();
+                    configuration.SetScreenSize(width, height);
+                });
 
             Game.Create(configuration);
             Game.Instance.Run();
             Game.Instance.Dispose();
+        }
+
+        private static (int width, int height) DefineScreenSize()
+        {
+            var settings = new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.All };
+            var font = Serializer.Load<IFont>(Constants.Font, false, settings);
+            return (Constants.Resolution.width / font.GlyphWidth, Constants.Resolution.height / font.GlyphHeight);
         }
     }
 }
