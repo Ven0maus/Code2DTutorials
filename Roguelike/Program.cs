da634332608ce95136a292d2c3e576f708acede6
venomaus <stefvandenbruel@outlook.com>
2025-01-10 11:47:08 +0100

    Add comment

--- Roguelike/Program.cs
+++ Roguelike/Program.cs
@@ -24,8 +24,10 @@ namespace Roguelike
                 {
                     f.UseCustomFont(Constants.Font);
 
-                    // Set screen size based on the font
-                    var (width, height) = DefineScreenSize();
+                    // A bit of a hacky solution, but we read the font json file to determine the width/height of the font glyphs
+                    // This way we can determine how many cells can fit in our resolution and we pass that as our screen size in cells.
+                    // We do it in ConfigureFonts because here the GameHost.Instance property is not null, which is needed for the serializer.
+                    var (width, height) = DefineScreenSizeByResolution();
                     configuration.SetScreenSize(width, height);
                 });
 
@@ -34,7 +36,7 @@ namespace Roguelike
             Game.Instance.Dispose();
         }
 
-        private static (int width, int height) DefineScreenSize()
+        private static (int width, int height) DefineScreenSizeByResolution()
         {
             var settings = new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.All };
             var font = Serializer.Load<IFont>(Constants.Font, false, settings);
